50404d65a219aacd3074efea30a63bcff9e9aba7
Hasse Hagen Johansen <hhj-github@musikcheck.dk>
2024-02-28 16:18:07 +0100

    first changes

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> { config = { allowUnfree = true; }; } }:
-
-pkgs.dockerTools.buildLayeredImage {
-  name = "roon-server";
-  config = {
-    Cmd = [ "${pkgs.roon-server}/bin/RoonServer" ];
-  };
-}
